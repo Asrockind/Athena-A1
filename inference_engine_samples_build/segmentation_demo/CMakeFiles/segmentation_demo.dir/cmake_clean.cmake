bde6969380ec7e25b0b0cb90681ebcb93fc11fd7
athena-series <Edwin_Hung@asrockind.com>
2020-01-08 12:05:42 +0800

    first commit

---
+++ inference_engine_samples_build/segmentation_demo/CMakeFiles/segmentation_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segmentation_demo.dir/main.cpp.o"
+  "../intel64/Release/segmentation_demo.pdb"
+  "../intel64/Release/segmentation_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/segmentation_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
